8a745ecbeb45f7daec9801b319f34c49631aeff8
Phyllis Kan <pyk9q7@gmail.com>
2019-05-08 21:53:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
+    puts "FizzBuzz"
   elsif num % 3 == 0
-    "Fizz"
+    puts "Fizz"
   elsif num % 5 == 0
-    "Buzz"
+    puts "Buzz"
   else
-    "nil"
+    puts "nil"
   end
 end
 
