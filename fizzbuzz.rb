88170620ca5fd5f83cf9933cb6d470a608979c02
Phyllis Kan <pyk9q7@gmail.com>
2019-05-08 22:08:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,15 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
+    return "Fizz"
+   
   elsif num % 3 == 0 && num % 5 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif num % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   else
-    puts "nil"
+    return "nil"
   end
 end
 
-fizzbuzz(3)
+fizzbuzz()
